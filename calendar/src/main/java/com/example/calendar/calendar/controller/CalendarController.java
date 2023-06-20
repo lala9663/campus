6a6e32abdf4b38f85fabb3baccede5bd47b8c97f
lala9663 <lala96632040@gmail.com>
2023-06-20 23:13:09 +0900

    calendar 배운 내용 정리

---
+++ calendar/src/main/java/com/example/calendar/calendar/controller/CalendarController.java
@@ -0,0 +1,37 @@
+package com.example.calendar.calendar.controller;
+
+
+import com.example.calendar.dto.Calendar;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class CalendarController {
+
+    private ArrayList<Calendar> calList=new ArrayList<>();
+
+    @GetMapping("/all/schedule")
+    public ArrayList<Calendar> allSchedule(){
+        return calList;
+    }
+
+    @GetMapping("/put/schedule")
+    public ArrayList<Calendar> allSchedule(Calendar cal){
+       boolean flag=true;
+        for(int i=0;i<calList.size();i++){
+            if(calList.get(i).getDate().equals(cal.getDate())){
+                calList.set(i,cal);
+                flag=false;
+            }
+        }
+        if(flag)
+        calList.add(cal);
+
+        return calList;
+    }
+
+
+
+}
